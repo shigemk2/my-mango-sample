70997088d5178a2d906cc0fd3eef69ea369337eb
shigemk2 <i.am.shige@gmail.com>
2016-04-24 18:33:34 +0900

    Add sample

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,17 @@
 package com.example
 
+import java.util.concurrent.TimeUnit
+import org.feijoas.mango.common.cache._
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val expensiveFnc = (str: String) => str.length
+    val cache = CacheBuilder.newBuilder()
+      .maximumSize(100)
+      .expireAfterWrite(10, TimeUnit.MINUTES)
+      .build(expensiveFnc)
+    println(cache("MyString"))
+    println(expensiveFnc("MyString"))
     println("Hello, world!")
   }
 }
